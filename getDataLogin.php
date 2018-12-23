3d8f6abf0b69abd7d36dbd86331fedc894aad277
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-12-23 20:15:27 +0100

    Update getDataLogin.php

--- getDataLogin.php
+++ getDataLogin.php
@@ -1,12 +1,11 @@
 <?php 
 
-$servername = "localhost"
 $hostname =" 3.83.23.191";
 $username = "newuser";
 $pass = "password";
 $dataBaseName = "freelancer";
 
-$connection = mysqli_connect($servername,$hostname, $username, $pass);
+$connection = mysqli_connect($hostname, $username, $pass);
 $selection = mysqli_select_db($connection, $dataBaseName);
 
 $mail = $_POST['userName'];
