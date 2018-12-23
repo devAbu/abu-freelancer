858910300fcf824f926eae776ab8c7484bd74b89
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-12-23 15:29:16 +0100

    Update profile-customer.php

--- profile-customer.php
+++ profile-customer.php
@@ -57,10 +57,10 @@
         <label>Jobs posted number:</label>
         <br>
         <?php
-        $hostname = "localhost";
-        $username = "root";
-        $pass = "";
-        $dataBaseName = "freelancer";
+       $hostname = "http://3.83.23.191";
+	$username = "newuser";
+	$pass = "password";
+	$dataBaseName = "freelancer";
 
         $connection = mysqli_connect($hostname, $username, $pass);
         $selection = mysqli_select_db($connection, $dataBaseName);
