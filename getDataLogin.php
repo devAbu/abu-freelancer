ca4e82f5c36a2981d5c38a45fbeb506f07949392
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-12-23 19:00:39 +0100

    Update getDataLogin.php

--- getDataLogin.php
+++ getDataLogin.php
@@ -21,12 +21,12 @@ echo "Password :   " .$password. "<br>";
 $success = true;
 
 if(!$connection){
-	echo "".mysqli_error();
+	echo "abu ".mysqli_error();
 	$success = false;
 }
 
 if(!$selection){
-	die(mysqli_error());
+	echo "combe ".mysqli_error();
 	$success = false;
 }
 
