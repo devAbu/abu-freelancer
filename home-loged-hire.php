38bc458d940b343f92970421f50e6b936c6005be
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-12-23 15:25:01 +0100

    Update home-loged-hire.php

--- home-loged-hire.php
+++ home-loged-hire.php
@@ -44,10 +44,10 @@
 
     <article style="margin-left:100px; max-width:880px;" >
 <?php
-        $hostname = "localhost";
-        $username = "root";
-        $pass = "";
-        $dataBaseName = "freelancer";
+        $hostname = "http://3.83.23.191";
+	$username = "newuser";
+	$pass = "password";
+	$dataBaseName = "freelancer";
 
         $connection = mysqli_connect($hostname, $username, $pass);
         $selection = mysqli_select_db($connection, $dataBaseName);
