3a315e9dd84c241c3815f6b7fa6052c2aa636f14
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-12-24 21:32:01 +0100

    Update ajaxProposal.php

--- ajaxProposal.php
+++ ajaxProposal.php
@@ -1,8 +1,8 @@
 <?php
     
-	define('DB_USER', 'root');
-	define('DB_PASSWORD', '');
-	define('DB_HOST', 'localhost');
+	define('DB_USER', 'newuser');
+	define('DB_PASSWORD', 'password');
+	define('DB_HOST', 'http://3.83.23.191');
 	define('DB_NAME', 'freelancer');
 
     $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die('could not connect'. mysqli_connect_error());
