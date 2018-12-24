eba8ccdbd0ccc6d11719c88e0df9ee5023875f03
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-12-24 22:01:44 +0100

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 	$hostname = "http://127.0.0.1";
 	$username = "newuser";
 	$pass = "password";
-	$dataBaseName = "freelancer";
+	$dataBaseName = "freelancer"; 
 
 	$connection = mysqli_connect($hostname, $username, $pass);
 	$selection = mysqli_select_db($connection, $dataBaseName);
