00fe45711455a6d789fab36fb11117f20eddc788
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-12-24 21:28:47 +0100

    Update profile-customer.php

--- profile-customer.php
+++ profile-customer.php
@@ -57,15 +57,7 @@
         <label>Jobs posted number:</label>
         <br>
         <?php
-       $hostname = "http://3.83.23.191";
-	$username = "newuser";
-	$pass = "password";
-	$dataBaseName = "freelancer";
-
-        $connection = mysqli_connect($hostname, $username, $pass);
-        $selection = mysqli_select_db($connection, $dataBaseName);
-
-        $success = true;
+      require 'config.php';
 
         if(!$connection){
     	die("connection failed ".mysqli_error());
